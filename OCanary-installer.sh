54de2e3fc32e5f455b02b36da3d70fa5eb849ed9
adaisy319 <37162867+adaisy319@users.noreply.github.com>
2019-08-21 10:26:48 -0400

    Update OCanary-installer.sh

--- OCanary-installer.sh
+++ OCanary-installer.sh
@@ -9,6 +9,10 @@ then
       # If the hostname is empty, do nothing. Set the hostname variable to use
       echo "Not changing hostname"
       hs=$(hostname)
+else
+# Validate the hostname provided matches the required pattern
+while [[ ! "$hs" =~ '[A-Za-z ]' ]]; do
+    read -p "Wrong hostname format. Re-enter using only A-Z, or a-z: " hs
 done
 
 # Reset the hostname
